70eb1d997e17ba626bcb9a82bfe146cf3b190692
Sandeep GC <sandeep_gc@hotmail.com>
2021-08-08 10:37:02 +1000

    added the readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Summary
+The user will be able to search the property for the particular location to invest.
+
+
+## Table of Contents
+- [User Story](#userstory)
+- [Functionality](#functionality)
+- [Acceptance Criteria](#acceptancecriteria)
+- [Technology Used](#technologyused)
+- [Screenshot](#screenshot)
+
+
+## User Story
+
+* AS AN tenant
+I WANT to be able to search property to rent.
+
+## Business Context
+
+Tenant who pay their weekly rent can get an opportunity to own the share of that property.
+
+## Functionality
+
+* As a tenant I can search a property for a particular suburb
+
+* I can sign up as a user.
+
+* Login as a user with the sign up email address
+
+## Technology Used
+
+* React for the front end.
+
+* Node.js and Express.js server.
+
+* MongoDB and the Mongoose ODM for the database.
+
+* Heroku (with data).
+
+## Screenshot![](./screenshot.PNG "Description goes here")
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -72,6 +72,21 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
+
+
+      // const apartmentData = items.map((apartment) => ({
+        
+      // //   bookId: book.id,
+      // listing: apartment.listingType,
+      //   image: book.volumeInfo.imageLinks?.thumbnail || ''
+
+
+      // //   authors: book.volumeInfo.authors || ['No author to display'],
+      // //   title: book.volumeInfo.title,
+      // //   description: book.volumeInfo.description,
+      // //   image: book.volumeInfo.imageLinks?.thumbnail || '',
+      // }));
+      // console.log(apartmentData);
       
 
       setSearchedBooks(bookData);
@@ -170,39 +185,7 @@ const SearchBooks = () => {
         </CardColumns>
       </Container>
 
-      <Container>
-        <h2>
-          {searchedApartments.length
-            ? `Viewing ${searchedApartments.length} results:`
-            : 'Search for your property'}
-        </h2>
-        <CardColumns>
-          {searchedApartments.map( (apartment) => {
-            return (
-              <Card key={apartment.type} border='dark'>
-                {/* {book.image ? (
-                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
-                ) : null}
-                {/* <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  {Auth.loggedIn() && (
-                    // <Button
-                    //   disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                    //   className='btn-block btn-info'
-                    //   onClick={() => handleSaveBook(book.bookId)}>
-                    //   {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                    //     ? 'This book has already been saved!'
-                    //     : 'Save this Book!'}
-                    // </Button>
-                  )}
-                </Card.Body> */} */}
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </Container>
+      
 
     </>
   );
